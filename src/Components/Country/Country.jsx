0977a38b86704218f76300d8b8836bdb146d8b24
MD. TAIFUR RAHMAN JASIM <taif.jnu@gmail.com>
2026-03-20 01:16:04 +0600

    Information added to countries

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 const Country = ({ country }) => {
 
+
+    const currencyMap = country.currencies.currencies;
+    const currencyName = Object.values(currencyMap).map(item => item.name);
+    const currencySymbol = Object.values(currencyMap).map(item => item.symbol);
+
     return (
         <div>
-            <h3>Name: </h3>
+
+            <img src={country.flags.flags.png} alt={country.flags.flags.al} />
+            <h5>Continent: {country.continents.continents}</h5>
+            <h3>Name:  {country.name.common} </h3>
+            <h4>Capital: {country.capital.capital}</h4>
+            <p>Population: {country.population.population}</p>
+            <p>Currencies:  {currencyName}(  {currencySymbol})</p>
+
         </div>
     );
 };
